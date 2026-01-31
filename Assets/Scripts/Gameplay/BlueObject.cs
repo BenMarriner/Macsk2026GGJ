74bda953ca874a43ebcde35c37fdde7c40c0a870
R0binicus <robinandloki@gmail.com>
2026-01-31 23:09:36 +1100

    Fully working moving platform
    - Moving platform properly moves the player on top of it in all directions
    - The player can also move while on platforms

--- Assets/Scripts/Gameplay/BlueObject.cs
+++ Assets/Scripts/Gameplay/BlueObject.cs
@@ -1,68 +1,66 @@
-using System.Collections;
 using UnityEngine;
 
 public class BlueObject : MaskChangeDetector
 {
     [SerializeField] private GameObject _movingPlatform;
-    [SerializeField] private Transform _platformEndPointA;
-    [SerializeField] private Transform _platformEndPointB;
+    [SerializeField] private WaypointPath _waypointPath;
     [SerializeField] private float _speed = 10f;
-    [SerializeField] private float _delay = 1f;
 
-    private Vector3 _currentPlatformDestination;
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
 
     private bool _isMoving = true;
 
-    IEnumerator MovePlayformCoroutine;
-
-    void Start()
+    private void Start()
     {
-        _movingPlatform.transform.position = _platformEndPointA.position;
-        _currentPlatformDestination = _platformEndPointA.position;
+        _movingPlatform.transform.position = _waypointPath.GetWaypoint(_targetWaypointIndex).transform.position;
+        TargetNextWaypoint();
+
         DisableBlueEffect();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isMoving)
+        {
+            return;
+        }
+
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = _elapsedTime / _speed;
+        _movingPlatform.transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
     }
 
     protected override void EnableBlueEffect()
     {
-        DebugLogger.Log("trigger true");
         _isMoving = !_effectReversed;
-
-        if (_isMoving)
-        {
-            StartCoroutine(MovePlatform());
-        }
     }
 
     protected override void DisableBlueEffect()
     {
-        DebugLogger.Log("trigger false");
         _isMoving = _effectReversed;
-
-        if (_isMoving)
-        {
-            StartCoroutine(MovePlatform());
-        }
     }
 
-    IEnumerator MovePlatform()
+    private void TargetNextWaypoint()
     {
-        while (_isMoving)
-        {
-            while ((_currentPlatformDestination - _movingPlatform.transform.position).sqrMagnitude > 0.01f)
-            {
-                if (!_isMoving)
-                {
-                    yield break;
-                }
-                _movingPlatform.transform.position = Vector3.MoveTowards(_movingPlatform.transform.position, 
-                _currentPlatformDestination, _speed * Time.deltaTime);
-                yield return null;
-            }
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
 
-            _currentPlatformDestination = _currentPlatformDestination == _platformEndPointA.position 
-            ? _platformEndPointB.position : _platformEndPointA.position;
+        _elapsedTime = 0f;
 
-            yield return new WaitForSeconds(_delay);
-        }
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        _timeToWaypoint = distanceToWaypoint / _speed;
     }
 }

da6f651928f9fba46179bdbb555b4e5f418ba870
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-02-01 11:30:20 +1100

    Can only interact with objects when wearing the green mask

--- Assets/Scripts/Gameplay/GreenObject.cs
+++ Assets/Scripts/Gameplay/GreenObject.cs
@@ -7,6 +7,8 @@ public class GreenObject : MaskChangeDetector
     private Material _defaultObjectMaterial;
     [SerializeField] private List<Renderer> _highlightMeshes;
 
+    private IInteractable _interactable;
+    
     private Renderer _objectRenderer;
 
     protected virtual void Start()
@@ -15,6 +17,11 @@ public class GreenObject : MaskChangeDetector
         if (_objectRenderer != null)
         {
             _defaultObjectMaterial = _objectRenderer.material;
+        }
+
+        if (_interactable == null && TryGetComponent(out IInteractable pairedInteractable))
+        {
+            _interactable = pairedInteractable;
         }
 
         Unhighlight();
@@ -33,6 +40,7 @@ public class GreenObject : MaskChangeDetector
 
     public virtual void Highlight()
     {
+        _interactable.SetCanBeInteracted(true);
         foreach (Renderer item in _highlightMeshes)
         {
             item.enabled = true;
@@ -41,6 +49,7 @@ public class GreenObject : MaskChangeDetector
 
     public virtual void Unhighlight()
     {
+        _interactable.SetCanBeInteracted(false);
         foreach (Renderer item in _highlightMeshes)
         {
             item.enabled = false;

a64971d83f91524bc90759d5af7354715b8fddc6
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-02-01 14:23:19 +1100

    Minor changes
    Lever starts in the correct position before animation starts. Properly position "Escape to menu" text for the canvas on the players camera.

--- Assets/Scripts/Gameplay/MaskManager.cs
+++ Assets/Scripts/Gameplay/MaskManager.cs
@@ -6,10 +6,16 @@ public class MaskManager : MonoBehaviour
 {
     [SerializeField] private MaskMode _currentMaskMode = MaskMode.NoMask;
     private int MaskIntMax = Enum.GetNames(typeof(MaskMode)).Length - 1;
+    
+    [SerializeField] private Material screenTint;
+    [SerializeField] private float redTintValue = .6f;
+    [SerializeField] private float blueTintValue = .5f;
+    [SerializeField] private float greenTintValue = .4f;
 
     protected virtual void OnEnable()
     {
         EventManager.RegisterEvent(EventKey.MASK_INPUT, SwitchMaskScroll);
+        ResetScreenTint();
     }
 
     protected virtual void OnDisable()
@@ -17,16 +23,13 @@ public class MaskManager : MonoBehaviour
         EventManager.DeregisterEvent(EventKey.MASK_INPUT, SwitchMaskScroll);
     }
 
-    void Start()
+    protected void ResetScreenTint()
     {
-        
+        screenTint.SetFloat("_Red", 0f);
+        screenTint.SetFloat("_Blue", 0f);
+        screenTint.SetFloat("_Green", 0f);
     }
-
-    void Update()
-    {
-        
-    }
-
+    
     void SwitchMaskScroll(object eventData)
     {
         if (eventData is not int) this.LogError("Event listener recieved incorrect data type!");
@@ -55,16 +58,42 @@ public class MaskManager : MonoBehaviour
         {
             case MaskMode.NoMask:
                 EventManager.TriggerEvent(EventKey.MUSIC, SoundType.NoMask);
+                ResetScreenTint();
                 break;
             case MaskMode.RedMask:
                 EventManager.TriggerEvent(EventKey.MUSIC, SoundType.RedMask);
+                SetRedScreenTint();
                 break;
             case MaskMode.GreenMask:
                 EventManager.TriggerEvent(EventKey.MUSIC, SoundType.GreenMask);
+                SetGreenScreenTint();
                 break;
             case MaskMode.BlueMask:
                 EventManager.TriggerEvent(EventKey.MUSIC, SoundType.BlueMask);
+                SetBlueScreenTint();
             break;
         }
     }
+    
+    private void SetRedScreenTint()
+    {
+        screenTint.SetFloat("_Red", redTintValue);
+        screenTint.SetFloat("_Blue", 0f);
+        screenTint.SetFloat("_Green", 0f);
+    }
+    
+    private void SetBlueScreenTint()
+    {
+        screenTint.SetFloat("_Red", 0f);
+        screenTint.SetFloat("_Blue", blueTintValue);
+        screenTint.SetFloat("_Green", 0f);
+    }
+    
+    private void SetGreenScreenTint()
+    {
+        screenTint.SetFloat("_Red", 0f);
+        screenTint.SetFloat("_Blue", 0f);
+        screenTint.SetFloat("_Green", greenTintValue);
+    }
+    
 }

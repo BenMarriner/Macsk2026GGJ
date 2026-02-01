9635297ba28e523c03f54d18482d9bd41b451a0c
Henry Kavadias-Barnes <bkyrneh@gmail.com>
2026-02-01 13:33:32 +1100

    Added Scene Change Interaction script

---
+++ Assets/Scripts/Gameplay/Interactions/SceneChangeInteract.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SceneChangeInteract : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int nextSceneIndex = 4;
+    public void Interact()
+    {
+        EventManager.TriggerEvent(EventKey.OPEN_SCENE, nextSceneIndex);
+    }
+
+    public void SetCanBeInteracted(bool val)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Highlight()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Unhighlight()
+    {
+        throw new System.NotImplementedException();
+    }
+}

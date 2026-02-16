dfed91493f41225b04f258e7794d22a6a6e81d5b
R0binicus <robinandloki@gmail.com>
2026-02-16 13:23:46 +1100

    Setup chest interact open trigger
    - Interacting with the chest will now play a chest open animation, before changing scenes

--- Assets/Scripts/Gameplay/Interactions/SceneChangeInteract.cs
+++ Assets/Scripts/Gameplay/Interactions/SceneChangeInteract.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class SceneChangeInteract : GreenObject, IInteractable
 {
-    [SerializeField] private int nextSceneIndex = 4;
+    private bool _activated = false;
+
+    [SerializeField] private AnimationClip _animationClip;
+    
+    [SerializeField] private Animator _animator;
+    [SerializeField] private int _nextSceneIndex = 4;
     public void Interact()
     {
-        EventManager.TriggerEvent(EventKey.OPEN_SCENE, nextSceneIndex);
+        if (_activated) return;
+        _activated = !_activated;
+        
+        _animator.Play(_animationClip.name);
+    }
+
+    // Triggered with animation event
+    public void ChangeScenes()
+    {
+        EventManager.TriggerEvent(EventKey.OPEN_SCENE, _nextSceneIndex);
     }
 
     public void SetCanBeInteracted(bool val)

8e7755086d83536fc4bbcd572896b2fc849d6b12
R0binicus <robinandloki@gmail.com>
2026-02-25 13:56:21 +1100

    Blue Object Update: Added BlueWaypointObject
    - Added BlueWaypointObject, and moved waypoint specific code to it, leaving BlueObject as a generic parent

--- Assets/Scripts/Gameplay/ColourObjects/BlueObject.cs
+++ Assets/Scripts/Gameplay/ColourObjects/BlueObject.cs
@@ -2,45 +2,14 @@ using UnityEngine;
 
 public class BlueObject : ColouredObject
 {
-    [SerializeField] private GameObject _movingPlatform;
-    [SerializeField] private WaypointPath _waypointPath;
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] protected float _speed = 10f;
 
-    private int _targetWaypointIndex;
-
-    private Transform _previousWaypoint;
-    private Transform _targetWaypoint;
-
-    private float _timeToWaypoint;
-    private float _elapsedTime;
-
-    private bool _isMoving = true;
-
-    private void Start()
+    protected bool _isMoving;
+    protected virtual void Start()
     {
         _defaultMaterialList = GetDefaultMaterialList(GetComponentsInChildren<Transform>());
-        _movingPlatform.transform.position = _waypointPath.GetWaypoint(_targetWaypointIndex).transform.position;
-        TargetNextWaypoint();
 
         SetBlueEffect(false);
-    }
-
-    private void FixedUpdate()
-    {
-        if (!_isMoving)
-        {
-            return;
-        }
-
-        _elapsedTime += Time.deltaTime;
-
-        float elapsedPercentage = _elapsedTime / _speed;
-        _movingPlatform.transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
-
-        if (elapsedPercentage >= 1)
-        {
-            TargetNextWaypoint();
-        }
     }
 
     protected override void SetBlueEffect(bool blueEnabled)
@@ -69,17 +38,5 @@ public class BlueObject : ColouredObject
 
             item.First.material = newMaterial;
         }
-    }
-
-    private void TargetNextWaypoint()
-    {
-        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
-        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
-        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
-
-        _elapsedTime = 0f;
-
-        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
-        _timeToWaypoint = distanceToWaypoint / _speed;
     }
 }
---
+++ Assets/Scripts/Gameplay/ColourObjects/BlueWaypointObject.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class BlueWaypointObject : BlueObject
+{
+    [SerializeField] private GameObject _movingPlatform;
+    [SerializeField] private WaypointPath _waypointPath;
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+
+    protected override void Start()
+    {
+        _defaultMaterialList = GetDefaultMaterialList(GetComponentsInChildren<Transform>());
+        _movingPlatform.transform.position = _waypointPath.GetWaypoint(_targetWaypointIndex).transform.position;
+        TargetNextWaypoint();
+
+        SetBlueEffect(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isMoving)
+        {
+            return;
+        }
+
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercentage = _elapsedTime / _speed;
+        _movingPlatform.transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+
+        if (elapsedPercentage >= 1)
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void TargetNextWaypoint()
+    {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0f;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+        _timeToWaypoint = distanceToWaypoint / _speed;
+    }
+}

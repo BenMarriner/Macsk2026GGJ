6c00c3c7f97d1aebfa21f6b30448219d7052f32f
R0binicus <robinandloki@gmail.com>
2026-03-18 15:36:47 +1100

    Post-Showcase Updates: Added ResetActivation
    - Added Added ResetActivation script
    - Moved TriggerWaypointObject reset functionality to ResetActivation
    - Added ObstacleElevatorResetter in level

---
+++ Assets/Scripts/Gameplay/ColourObjects/Interactions/Activations/ResetActivation.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetActivation : AbstractActivation
+{
+    [SerializeField] protected List<ColouredObject> _resetColourObjectList;
+    [SerializeField] protected bool _resetOnPlayerDeath = false;
+
+    protected void OnEnable()
+	{
+		EventManager.RegisterEvent(EventKey.PLAYER_DIED, PlayerDiedhandler);
+	}
+
+	protected void OnDisable()
+	{
+		EventManager.DeregisterEvent(EventKey.PLAYER_DIED, PlayerDiedhandler);
+	}
+    
+    public override void Activate()
+    {
+        Reset();
+    }
+
+    protected virtual void PlayerDiedhandler(object eventData)
+    {
+        if (!_resetOnPlayerDeath) return;
+        Reset();
+    }
+
+    [ContextMenu("Reset Objects")]
+    public void Reset()
+    {
+        foreach (ColouredObject item in _resetColourObjectList)
+        {
+            if (!item) continue;
+            item.Reset();
+        }
+    }
+}
